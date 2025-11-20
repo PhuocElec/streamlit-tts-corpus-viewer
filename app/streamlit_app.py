2d5ffd7f05571fd25519eca326c5c39e6f52df28
phuocletel4vn <vinhphuoc@pitel.vn>
2025-11-20 17:50:06 +0700

    initial base source

---
+++ app/streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def main() -> None:
+    st.set_page_config(
+        page_title="Streamlit TTS Corpus Viewer",
+        layout="wide",
+    )
+
+
+if __name__ == "__main__":
+    main()
